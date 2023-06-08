7a9e794b69d66cdfb250ddbc2716ec879afb7a98
Vinicius <vinnicius.jimenez@gmail.com>
2023-06-08 17:13:06 -0300

    truncate text

--- src/components/highlight-text/index.tsx
+++ src/components/highlight-text/index.tsx
@@ -1,28 +1,39 @@
 import { FC, memo, useCallback } from "react";
-import { Text, View } from "react-native";
+import { StyleProp, Text, View, ViewStyle } from "react-native";
 import { styles } from "./style";
 
 interface Props{
     search: string
     children: string
+    style?: StyleProp<ViewStyle>
 }
 
-const Highlight: FC<Props> = ({ search, children }) => {
+const Highlight: FC<Props> = ({ search, children, style }) => {
     const regex = new RegExp(`(${search})`, 'gi')
     const parts = children.split(regex)
 
     const isEqualToSearch = useCallback((string: string) =>  string.toLowerCase() === search.toLowerCase(), [ search ])
+    
+    const truncateText = useCallback((text: string, maxLength: number) => {
+      if (text.length <= maxLength) {
+          return text;
+        }
+        return text.slice(0, maxLength) + '...';
+    }, [])
+    
 
     return (
-        <View style={styles.container}>
-            {parts.map((part, index) => (
-                <Text
-                    key={index}
-                    style={isEqualToSearch(part) ? styles.highlight : null}
-                >
-                    {part}
-                </Text>
-            ))}
+        <View style={[styles.container, style]}>
+            <Text numberOfLines={1} ellipsizeMode="tail">
+                {parts.map((part, index) => (
+                    <Text
+                        key={index}
+                        style={isEqualToSearch(part) ? styles.highlight : null}
+                    >
+                        {truncateText(part, 40)}
+                    </Text>
+                ))}
+            </Text>
         </View>
     )
 }
